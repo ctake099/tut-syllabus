e9dc578a37ae1e4c38bbb8e9d0a1d6aeb838d37d
takeru.shingu <takeru.shingu@town.biz>
2025-05-24 18:03:39 +0900

    ipv4に変更

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -5,7 +5,18 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
+// ✅ Transaction Pooler 対応のオプションを指定
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient()
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+    log: ['query'],
+  })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
